8ba39ffd2e29c07310efa4dde08424ef1a2b26b0
Ayush Gupta <meetag007@gmail.com>
2023-08-20 23:45:08 +0530

    random

---
+++ Baazar/PolicyBazar/src/main/java/com/example/demo/PolicyBazarApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyBazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicyBazarApplication.class, args);
+	}
+
+}
